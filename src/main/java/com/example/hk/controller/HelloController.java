27c588ee71ae5472bf339a2116db5bd0aa9c6652
huangkai <457854314@qq.com>
2021-05-22 00:41:48 +0800

    重构代码版本

--- src/main/java/com/example/hk/controller/HelloController.java
+++ src/main/java/com/example/hk/controller/HelloController.java
@@ -1,20 +1,19 @@
 package com.example.hk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.example.hk.exception.ServiceException.PLAN_ALREADY_RELEASE;
 
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     public String say() {
-        if (true){
-            throw PLAN_ALREADY_RELEASE;
-        }
         return "index";
     }
 
